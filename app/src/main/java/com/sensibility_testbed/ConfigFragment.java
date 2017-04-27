c75fde7033be28190d323ec95a907c9832837567
MD Kabir <msk610@nyu.edu>
2017-04-26 23:49:47 -0400

    Parse and display networks using misc service along with sliders

--- app/src/main/java/com/sensibility_testbed/ConfigFragment.java
+++ app/src/main/java/com/sensibility_testbed/ConfigFragment.java
@@ -1,5 +1,7 @@
 package com.sensibility_testbed;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
@@ -11,6 +13,7 @@ import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.snakei.MiscInfoService;
 import com.snakei.SensorService;
 
 import org.json.JSONArray;
@@ -22,6 +25,7 @@ import org.json.JSONObject;
  */
 
 public class ConfigFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,13 +34,137 @@ public class ConfigFragment extends Fragment {
         View configContainer = inflater.inflate(R.layout.config, null);
         LinearLayout layout = (LinearLayout) configContainer.findViewById(R.id.sensorList);
 
-        // instantiate and initialize sensor service
+        // make the ui labels for different categories
+        TextView networkTitles = new TextView(getActivity());
+        networkTitles.setText("Networks");
+        TextView sensorTitles = new TextView(getActivity());
+        sensorTitles.setText("Sensors");
+
+        //set label style
+        LinearLayout.LayoutParams labelParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        labelParams.setMargins(20, 40, 0, 0);
+
+        networkTitles.setTypeface(networkTitles.getTypeface(), Typeface.BOLD);
+        networkTitles.setPaintFlags(networkTitles.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        networkTitles.setLayoutParams(labelParams);
+
+        sensorTitles.setTypeface(sensorTitles.getTypeface(), Typeface.BOLD);
+        sensorTitles.setPaintFlags(sensorTitles.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        sensorTitles.setLayoutParams(labelParams);
+
+        // instantiate and initialize misc and sensor service
         SensorService sensorService = new SensorService();
+        MiscInfoService miscService = new MiscInfoService();
         sensorService.init(getContext());
+        miscService.init(getContext());
+
+        // first add the networks
+        layout.addView(networkTitles);
+
+        try {
+            // get the networks list
+            String networks = miscService.getNetworkInfo();
+            // System.out.println(sensors);
+
+            //parse the json array and go through each sensor
+            JSONArray networkArray = new JSONArray(networks);
+            for (int i = 0; i < networkArray.length(); i++) {
+                JSONObject networkObject = networkArray.getJSONObject(i);
+
+                // get the sensor name and power
+                String name = networkObject.get("type_name").toString();
+                String powerString = "50";
+                Double powerDouble = Double.parseDouble(powerString);
+                String type = "network";
+                int power = powerDouble.intValue();
+
+                // make the ui components
+                TextView networkLabel = new TextView(getActivity());
+                TextView accuracy = new TextView(getActivity());
+                TextView count = new TextView(getActivity());
+
+                SeekBar networkBar = new SeekBar(getActivity());
+                SeekBar networkBar2 = new SeekBar(getActivity());
+
+                // add listener for seekbar
+                SeekBar.OnSeekBarChangeListener networkBarListener = new SeekBar.OnSeekBarChangeListener() {
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        //Executed when progress is changed
+                        // System.out.println(progress);
+                    }
+                };
+
+
+                // add listener for seekbar2
+                SeekBar.OnSeekBarChangeListener networkBarListener2 = new SeekBar.OnSeekBarChangeListener() {
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        //Executed when progress is changed
+                        // System.out.println(progress);
+                    }
+                };
+                networkBar.setOnSeekBarChangeListener(networkBarListener);
+                networkBar2.setOnSeekBarChangeListener(networkBarListener2);
+
+                // update info for ui
+                networkLabel.setText(name);
+                networkLabel.setGravity(Gravity.RIGHT);
+                networkLabel.setTypeface(networkLabel.getTypeface(), Typeface.BOLD);
+                networkLabel.setLayoutParams(labelParams);
+
+                accuracy.setText("Accuracy:");
+                LinearLayout.LayoutParams labelParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                labelParams2.setMargins(30, 40, 0, 0);
+                accuracy.setLayoutParams(labelParams2);
+
+                count.setText("Count:");
+                LinearLayout.LayoutParams labelParams3 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                labelParams3.setMargins(40, 40, 0, 0);
+                count.setLayoutParams(labelParams3);
+
+                // add the network label
+                layout.addView(networkLabel);
+                layout.addView(accuracy);
+                // add the network bar
+                networkBar.setProgress(power);
+                layout.addView(networkBar);
+                layout.addView(count);
+                networkBar2.setProgress(power);
+                layout.addView(networkBar2);
+
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //now add the sensor title
+        layout.addView(sensorTitles);
 
         try {
             // get the sensor list
             String sensors = sensorService.getSensorList();
+            // System.out.println(sensors);
 
             //parse the json array and go through each sensor
             JSONArray sensorArray = new JSONArray(sensors);
@@ -47,11 +175,16 @@ public class ConfigFragment extends Fragment {
                 String name = sensorObject.get("name").toString();
                 String powerString = sensorObject.get("power").toString();
                 Double powerDouble = Double.parseDouble(powerString);
+                String type = sensorObject.get("string_type").toString();
                 int power = powerDouble.intValue();
 
                 // make the ui components
                 TextView sensorLabel = new TextView(getActivity());
+                TextView accuracy = new TextView(getActivity());
+                TextView count = new TextView(getActivity());
+
                 SeekBar sensorBar = new SeekBar(getActivity());
+                SeekBar sensorBar2 = new SeekBar(getActivity());
 
                 // add listener for seekbar
                 SeekBar.OnSeekBarChangeListener sensorBarListener = new SeekBar.OnSeekBarChangeListener() {
@@ -70,19 +203,54 @@ public class ConfigFragment extends Fragment {
                         // System.out.println(progress);
                     }
                 };
+
+
+                // add listener for seekbar2
+                SeekBar.OnSeekBarChangeListener sensorBarListener2 = new SeekBar.OnSeekBarChangeListener() {
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        //Executed when progress is changed
+                        // System.out.println(progress);
+                    }
+                };
                 sensorBar.setOnSeekBarChangeListener(sensorBarListener);
+                sensorBar2.setOnSeekBarChangeListener(sensorBarListener2);
 
                 // update info for ui
                 sensorLabel.setText(name);
                 sensorLabel.setGravity(Gravity.RIGHT);
-                LinearLayout.LayoutParams labelParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                labelParams.setMargins(20, 40, 0, 0);
+                sensorLabel.setTypeface(sensorLabel.getTypeface(), Typeface.BOLD);
                 sensorLabel.setLayoutParams(labelParams);
+
+                accuracy.setText("Accuracy:");
+                LinearLayout.LayoutParams labelParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                labelParams2.setMargins(30, 40, 0, 0);
+                accuracy.setLayoutParams(labelParams2);
+
+                count.setText("Count:");
+                LinearLayout.LayoutParams labelParams3 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                labelParams3.setMargins(40, 40, 0, 0);
+                count.setLayoutParams(labelParams3);
+
                 // add the sensor label
                 layout.addView(sensorLabel);
+                layout.addView(accuracy);
                 // add the sensor bar
                 sensorBar.setProgress(power);
                 layout.addView(sensorBar);
+                layout.addView(count);
+                sensorBar2.setProgress(power);
+                layout.addView(sensorBar2);
+
             }
 
 
